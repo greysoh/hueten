a8d1d4348c952fa27dba51ca1eb4bc751d982925
greysoh <greysoh@pm.me>
2022-06-01 08:04:45 -0700

    feature: Adds titlebar.

--- app/preloader.js
+++ app/preloader.js
@@ -1,3 +1,7 @@
+const { contextBridge, ipcRenderer } = require("electron");
+const titlebar = require("./pages/titlebar");
+
 addEventListener("DOMContentLoaded", async function() {
-    
+    titlebar();
+    ipcRenderer.send("ready");
 })
--- index.js
+++ index.js
@@ -1,16 +1,45 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const { join } = require("path");
+const conf = require("./conf.api");
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
+  let windowConfig = {
+    width: 300,
     height: 600,
+    frame: false,
     webPreferences: {
-        preload: join(__dirname, "/app/preloader.js")
-    }
-  });
+      preload: join(__dirname, "/app/preloader.js")
+    },
+  }
+  const win = new BrowserWindow(windowConfig);
+
+  win.hide();
 
   win.loadFile(join(__dirname + "./app/pages/index.html"));
+
+  ipcMain.on("ready", function () {
+    win.show();
+  });
+
+  ipcMain.on("minimize", function () {
+    win.minimize();
+  });
+
+  ipcMain.on("maximize", function () {
+    win.maximize();
+  });
+
+  ipcMain.on("unmaximize", function () {
+    win.unmaximize();
+  });
+
+  ipcMain.on("close", function () {
+    win.close();
+  });
+
+  win.onBeforeUnload = () => {
+      win.removeAllListeners();
+  }
 };
 
 app.on("window-all-closed", () => {
