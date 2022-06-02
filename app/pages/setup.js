25a0866340a2a69ee5a68cd6755ca4d30342b887
greysoh <greysoh@pm.me>
2022-06-02 15:31:01 -0700

    feature: Adds custom URL loading.

--- app/pages/setup.js
+++ app/pages/setup.js
@@ -1,12 +1,44 @@
 const HueAPI = require("../../hue.api");
 const hue = new HueAPI("Hueten");
 const { contextBridge, ipcRenderer } = require("electron");
+let isBridgeURLOverwritten = false;
+
+document.addEventListener("keypress", function(e) {
+    if (e.key.toLowerCase() == "e") {
+        isBridgeURLOverwritten = true;
+    }
+})
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 module.exports = async function() {
     if (localStorage.getItem("bridgeUrl") != null) {
         hue.connection.setBridgeURL(localStorage.getItem("bridgeUrl"));
     } else {
         await localStorage.setItem("bridgeUrl", await hue.connection.getBridgeURL());
+    }
+
+    if (isBridgeURLOverwritten) {
+        let input = document.createElement("input");
+        input.type = "text";
+        input.addEventListener("keypress", function(e) {
+            if (e.key == "Enter") {
+                localStorage.setItem("bridgeUrl", input.value);
+                hue.connection.setBridgeURL(localStorage.getItem("bridgeUrl"));
+                isBridgeURLOverwritten = false;
+            }
+        })
+
+        document.getElementsByClassName("text")[0].innerHTML = "";
+        document.getElementsByClassName("text")[0].appendChild(input);
+
+        while (isBridgeURLOverwritten) {
+            await sleep(100);
+        }
+
+        document.getElementsByClassName("text")[0].innerHTML = "";
     }
 
     document.getElementsByClassName("text")[0].innerText = "Please click the blue Pair button.";
